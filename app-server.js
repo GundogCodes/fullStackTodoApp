25de71b2d764662d3710b5726b03fa4c2e6e7372
Gunish <gunishsharma20@gmail.com>
2023-08-09 19:29:47 -0600

    changed title and engine node verison

--- app-server.js
+++ app-server.js
@@ -16,7 +16,7 @@ app.use((req,res,next) => {
 
 app.use(logger('dev'))
 app.use(favicon(path.join(__dirname, 'public','img', 'logo.png')))
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public'))) 
 app.use('/api/todos', require('./routes/api/todos'))
 
 app.get('*', (req,res)=>{
